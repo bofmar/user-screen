eb54a37c31f39c704d512b52a650dd159df54ccd
bofmar <bofmar@outlook.com>
2022-11-10 18:34:04 +0200

    Add game total and completion rating

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,15 +5,21 @@ export default class UserCard extends Component {
     super(props);
   }
 
+  getPercentageBeaten(total, beaten) {
+    return (beaten * 100 / total).toFixed(2);
+  }
+
   render() {
     const { id, username, avatar, gamesBeaten, gamesInBacklog } = this.props.user;
     return (
       <div>
         <img src={avatar} />
         <h1>{username.toUpperCase()}</h1>
-        <p>{`User ID: ${id}`}</p>
-        <p>{`Games beate: ${gamesBeaten}`}</p>
-        <p>{`Games in backlog: ${gamesInBacklog}`}</p>
+        <p>User ID: {id}</p>
+        <p>Games beaten: {gamesBeaten}</p>
+        <p>Games in backlog: {gamesInBacklog}</p>
+        <p>Total games: {gamesBeaten + gamesInBacklog}</p>
+        <p>Completion Percentage: {this.getPercentageBeaten((gamesBeaten + gamesInBacklog), gamesBeaten)}%</p>
       </div>
     )
   }
