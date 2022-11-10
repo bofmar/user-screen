11fdc5a33dc4dd95cfca4656c20a96f1e843080a
bofmar <bofmar@outlook.com>
2022-11-10 20:18:51 +0200

    Finish applying styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,16 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <h1>Backlog.it</h1>
-      <input type='search' placeholder='Search For Your Favorite Games...' />
-      <a>Forum</a>
-      <a>Stats</a>
-      <a>Submit</a>
-      <a>Logout</a>
+      <div className='left-side'>
+        <h1>Backlog.it</h1>
+        <input type='search' placeholder='Search For Your Favorite Games...' />
+      </div>
+      <div className='links'>
+        <a>Forum</a>
+        <a>Stats</a>
+        <a>Submit</a>
+        <a>Logout</a>
+      </div>
     </header>
   )
 }
